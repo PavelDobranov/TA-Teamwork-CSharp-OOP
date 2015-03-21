9e7d8691f22d2444c836b7e5be648a23004d62de
Адриан Дилов <adrian.dilov@abv.bg>
2015-03-21 15:53:00 +0200

    Basic skeleton

---
+++ Academy/Academy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    class Academy
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Academy/Curses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Academy
+{
+    public class Curses
+    {
+        public Curses(string name, DateTime start, DateTime end, CourseType type)
+        {
+ 
+        }
+    }
+}
