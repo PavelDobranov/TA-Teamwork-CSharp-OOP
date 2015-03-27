6ed1d03c88c18f36c23ac77da7555f1dd655ed82
kjifw <kjifw@kjifw.kjifw>
2015-03-27 19:14:14 +0200

    Added console app for testing purposes

---
+++ SourceCode/AcademySystemConsoleTest/AcademySystemConsoleTestClass.cs
@@ -0,0 +1,21 @@
+﻿namespace AcademySystemConsoleTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    using AcademySystem.Humans;
+    using AcademySystem.Interfaces;
+    using AcademySystem.Models;
+    using AcademySystem.Training;
+
+    public class AcademySystemConsoleTestClass
+    {
+        static void Main(string[] args)
+        {
+            // created for testing purposes
+        }
+    }
+}
