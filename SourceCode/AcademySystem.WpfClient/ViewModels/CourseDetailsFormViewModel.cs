2c001271db0358899ba6a87b84ade84977c044e9
juvemar <martoo.atanasov@gmail.com>
2015-04-07 17:39:01 +0300

    CourseDetailsFormView added

---
+++ SourceCode/AcademySystem.WpfClient/ViewModels/CourseDetailsFormViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace AcademySystem.WpfClient.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+    
+    using AcademySystem.WpfClient.Behavior;
+    using AcademySystem.Models.Training;
+
+    public class CourseDetailsFormViewModel : BaseVewModel
+    {
+        private ICommand subscribeCommand;
+
+        public FreeCourse FreeCourse {get; set;}
+
+        public ICommand Subscribe
+        {
+            get
+            {
+                if (this.subscribeCommand == null)
+                {
+                    this.subscribeCommand = new RelayCommand(this.HandleLoginCommand);
+                }
+
+                return this.subscribeCommand;
+            }
+        }
+
+        private void HandleLoginCommand(object parameter)
+        {
+            
+        }
+    }
+}
