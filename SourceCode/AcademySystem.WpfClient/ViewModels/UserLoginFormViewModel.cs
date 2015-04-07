08db14d6d55091c3f0f311370bff144c27f79275
kjifw <kjifw@kjifw.kjifw>
2015-04-07 15:06:44 +0300

    Added login view

---
+++ SourceCode/AcademySystem.WpfClient/ViewModels/UserLoginFormViewModel.cs
@@ -0,0 +1,47 @@
+﻿
+namespace AcademySystem.WpfClient.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+    using AcademySystem.WpfClient.Behavior;
+
+    public class UserLoginFormViewModel : BaseVewModel
+    {
+        private ICommand loginCommand;
+        private string message;
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Msg
+        {
+            get
+            {
+                return this.message;
+            }
+            set
+            {
+                this.message = value;
+            }
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (this.loginCommand == null)
+                {
+                    this.loginCommand = new RelayCommand(this.HandleLoginCommand);
+                }
+
+                return this.loginCommand;
+            }
+        }
+
+        private void HandleLoginCommand(object parameter)
+        {
+            this.Msg = string.Format("Username: {0}, \nPassowrd: {1}", this.Username, this.Password);
+        }
+    }
+}
