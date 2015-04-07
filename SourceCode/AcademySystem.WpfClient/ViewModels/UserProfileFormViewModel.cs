efd3ca1da171baf5bbadc93a249be6b5a531a7df
NikolaiMishev <nikolaimishev@gmail.com>
2015-04-07 20:01:15 +0300

    Something changed

---
+++ SourceCode/AcademySystem.WpfClient/ViewModels/UserProfileFormViewModel.cs
@@ -0,0 +1,37 @@
+﻿
+namespace AcademySystem.WpfClient.ViewModels
+{
+    using System;
+    using System.Windows.Input;
+    using AcademySystem.WpfClient.Behavior;
+    using AcademySystem.Models;
+    using System.Windows;
+    
+    public class UserProfileFormViewModel : BaseVewModel
+    {
+        private ICommand addPictureComand;
+        private User user = new User();
+        
+        public User User { get; set; }
+       
+        
+        public ICommand AddPicture
+        {
+            get
+            {
+                if (this.addPictureComand == null)
+                {
+                    this.addPictureComand = new RelayCommand(this.HandleAddPictureCommand);
+                }
+
+                return this.addPictureComand;
+            }
+        }
+
+        private void HandleAddPictureCommand(object parameter)
+        {
+            //TODO: using System.Windows;
+            MessageBox.Show("Adding picture - Impossible!");
+        }
+    }
+}
