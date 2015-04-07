0389e24cdc7f64e13c980a2c70709167fdbbe172
Aleksander Angelov <Obelixx@users.noreply.github.com>
2015-04-07 17:10:56 +0300

    CourcesList edits

--- SourceCode/AcademySystem.WpfClient/Views/CourcesListFormView.xaml.cs
+++ SourceCode/AcademySystem.WpfClient/Views/CourcesListFormView.xaml.cs
@@ -13,17 +13,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using AcademySystem.Models.Training;
+
 namespace AcademySystem.WpfClient.Views
 {
     /// <summary>
     /// Interaction logic for CourcesListFormView.xaml
     /// </summary>
-    public partial class CourcesListFormView : Page
+    public partial class CourcesListFormView : UserControl
     {
         public CourcesListFormView()
         {
             InitializeComponent();
-            //Cources.ItemsSource = TrainingClasses;
+            List<Training> CourcesList = new List<Training>();
+
+            #region testing
+            List<Training> TestCourcesList = new List<Training>();
+            TestCourcesList.Add(new FreeCourse("Name1", "Cat1", DateTime.Now, DateTime.Now.AddDays(1), "desc"));
+            TestCourcesList.Add(new FreeCourse("Name2", "Cat1133", DateTime.Now, DateTime.Now.AddDays(1), "desc"));
+            TestCourcesList.Add(new FreeCourse("Name3", "Cat23", DateTime.Now, DateTime.Now.AddDays(1), "descdescdescdescdesc"));
+            TestCourcesList.Add(new FreeCourse("Name4", "Cat123", DateTime.Now, DateTime.Now.AddDays(1), "descdescdescdescdescdescdescdescdescdesc"));
+            
+            #endregion
+
+            CourcesList.AddRange(TestCourcesList);
+            Cources.ItemsSource = CourcesList;
         }
     }
 }
